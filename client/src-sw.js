db3751f8af4f9003d834e54c352238da6c7f6a2f
Andrew McInally <realfunnyestate@yahoo.com>
2022-05-29 11:56:11 +0200

    debugging

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,11 +1,11 @@
-const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
-const { registerRoute } = require('workbox-routing');
-const { CacheableResponsePlugin } = require('workbox-cacheable-response');
-const { ExpirationPlugin } = require('workbox-expiration');
-const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+const { offlineFallback, warmStrategyCache } = require('workbox-recipes')
+const { StaleWhileRevalidate, CacheFirst } = require('workbox-strategies')
+const { registerRoute } = require('workbox-routing')
+const { CacheableResponsePlugin } = require('workbox-cacheable-response')
+const { ExpirationPlugin } = require('workbox-expiration')
+const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute')
 
-precacheAndRoute(self.__WB_MANIFEST);
+precacheAndRoute(self.__WB_MANIFEST)
 
 const pageCache = new CacheFirst({
   cacheName: 'page-cache',
@@ -17,26 +17,23 @@ const pageCache = new CacheFirst({
       maxAgeSeconds: 30 * 24 * 60 * 60,
     }),
   ],
-});
+})
 
 warmStrategyCache({
   urls: ['/index.html', '/'],
   strategy: pageCache,
-});
+})
 
-registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+registerRoute(({ request }) => request.mode === 'navigate', pageCache)
 
-// TODO: Implement asset caching
 registerRoute(
-  // callback function that will filter the requests we want to cache
-  ({ request }) => ["style", "script", "worker"].includes(request.destination),
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
   new StaleWhileRevalidate({
-    // Name of cache storage.
-    cacheName: "asset-cache",
+    cacheName: 'asset-cache',
     plugins: [
       new CacheableResponsePlugin({
         statuses: [0, 200],
       }),
     ],
   })
-);
+)
