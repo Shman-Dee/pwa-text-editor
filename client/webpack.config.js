534a09ac8ab7dff4e3cf7c283347c4b5eee7a15c
Andrew McInally <realfunnyestate@yahoo.com>
2022-05-29 11:55:23 +0200

    trouble shooting

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,50 +1,44 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const WebpackPwaManifest = require("webpack-pwa-manifest");
-const path = require("path");
-const { InjectManifest } = require("workbox-webpack-plugin");
-
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const WebpackPwaManifest = require('webpack-pwa-manifest')
+const path = require('path')
+const { InjectManifest } = require('workbox-webpack-plugin')
 
 module.exports = () => {
   return {
-    mode: "development",
+    mode: 'development',
     entry: {
-      main: "./src/js/index.js",
-      install: "./src/js/install.js",
+      main: './src/js/index.js',
+      install: './src/js/install.js',
     },
     output: {
-      filename: "[name].bundle.js",
-      path: path.resolve(__dirname, "dist"),
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
       new HtmlWebpackPlugin({
-        template: "./index.html",
-        title: "JATE",
+        template: './index.html',
+        title: 'JATE',
       }),
-      // injects custom service work from src-sw.js
-
-      new InjectManifest({
-        swSrc: "./src-sw.js",
-        swDest: "src-sw.js",
-      }),
-      // creates a manifest.json file
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: "JATE",
-        description: "another text editor",
-        background_color: "#225ca3",
-        theme_color: "#225ca3",
-        start_url: "/",
-        publicPath: "/",
+        name: 'JATE',
+        description: 'Just another text editor',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
         icons: [
           {
-            src: path.resolve("src/images/logo.png"),
+            src: path.resolve('src/images/logo.png'),
             sizes: [96, 128, 192, 256, 384, 512],
-            destination: path.join("assets", "icons"),
+            destination: path.join('assets', 'icons'),
           },
         ],
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
       }),
     ],
 
@@ -52,27 +46,27 @@ module.exports = () => {
       rules: [
         {
           test: /\.css$/i,
-          use: ["style-loader", "css-loader"],
+          use: ['style-loader', 'css-loader'],
         },
         {
           test: /\.(png|svg|jpg|jpeg|gif)$/i,
-          type: "asset/resource",
+          type: 'asset/resource',
         },
         {
           test: /\.m?js$/,
           exclude: /node_modules/,
           use: {
-            loader: "babel-loader",
+            loader: 'babel-loader',
             options: {
-              presets: ["@babel/preset-env"],
+              presets: ['@babel/preset-env'],
               plugins: [
-                "@babel/plugin-proposal-object-rest-spread",
-                "@babel/transform-runtime",
+                '@babel/plugin-proposal-object-rest-spread',
+                '@babel/transform-runtime',
               ],
             },
           },
         },
       ],
     },
-  };
-};
+  }
+}
